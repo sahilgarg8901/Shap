dee3a9d6ec07b7a7a174d156c5e14630b854ab2f
Sahil Garg <sahilgarg8901@gmail.com>
2018-05-19 18:30:39 +0530

    main activity initialized and linked

--- app/src/main/java/com/example/sahil/shap/StartActivity.kt
+++ app/src/main/java/com/example/sahil/shap/StartActivity.kt
@@ -1,12 +1,18 @@
 package com.example.sahil.shap
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_start.*
 
 class StartActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+        getStartedBtn.setOnClickListener{
+            val leagueIntent = Intent(this, MainActivity::class.java)
+            startActivity(leagueIntent)
+        }
     }
 }
